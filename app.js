f068e0cab35a54e3876ee444a1d082645292fd2d
John Desrosiers <john.desro@gmail.com>
2019-01-21 07:53:22 -0800

    Serve angular app as default

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -12,9 +11,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const clientDir = path.join(__dirname, 'public');
+
+app.use(express.static(clientDir));
+
+app.use('/api', apiRouter);
+
+app.get('*', (req, res) => res.sendFile(path.join(clientDir, 'index.html')));
 
 module.exports = app;
