3b03cd10eb5a9bb116e957e8a0d18b3d802548dc
John Desrosiers <john.desro@gmail.com>
2019-01-21 08:27:40 -0800

    Create GET and POST routes for authors

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const authors = require('../authors')
+
+router.get('/authors', authors.getAuthors);
+router.post('/authors', authors.addAuthor)
 
 module.exports = router;
